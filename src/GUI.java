bda97dfdce50ef877726eb1b6a2d49ab52f07d35
Zende-Magloire <89619545+Zende-Magloire@users.noreply.github.com>
2022-04-08 18:44:26 -0400

    day 2

--- src/GUI.java
+++ src/GUI.java
@@ -3,6 +3,15 @@ import java.awt.*;
 
 public class GUI extends JFrame
 {
+    //search
+    private JPanel TopPanel;
+    private JPanel BottomPanel;
+    private JTextField LBL_name;
+    private JTextField LBL_ID;
+    private JButton BTN_Search;
+    private JButton BTN_N_Search;
+    private JSplitPane splitPane;
+
     public static void main (String[] args)
     {
         EventQueue.invokeLater(
@@ -26,10 +35,33 @@ public class GUI extends JFrame
     {
         setTitle("Stetson Striders Database");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(512, 256);
-
+        setSize(900, 600);
         setLocationRelativeTo(null);
         setVisible(true);
-    }
+
+        //search panel
+        JPanel TopPanel = new JPanel();
+        LBL_name = new JTextField(10);
+        LBL_ID = new JTextField(10);
+        BTN_N_Search = new JButton("Search");
+        BTN_Search = new JButton("Search");
+        TopPanel.add(new JLabel("Search by name: "));
+        TopPanel.add(LBL_name);
+        TopPanel.add(BTN_N_Search);
+        TopPanel.add(new JLabel("Search by ID: "));
+        TopPanel.add(LBL_ID);
+        TopPanel.add(BTN_Search);
+//        BTN_N_Search.addActionListener(this);
+//        BTN_Search.addActionListener(this);
+
+        add(TopPanel, BorderLayout.NORTH);
+
+        //split pane
+        JScrollPane pnlList = new JScrollPane();
+        pnlList.setLayout(new ScrollPaneLayout());
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, pnlList, TopPanel);
+        splitPane.setDividerLocation(150);
+        add(splitPane, BorderLayout.CENTER);
+     }
 }
 
